b754ca37d3efdc9ec63a24021743feee1ac33648
Imam Hossain <imam2889@gmail.com>
2018-02-25 18:42:22 +0600

    How toString method will be Workedd

---
+++ ToString/Person.java
@@ -0,0 +1,16 @@
+package ToString;
+
+public class Person {
+ String name;
+ int age;
+ public Person(String name,int age) {
+	this.name=name;
+	this.age=age;
+	 // TODO Auto-generated constructor stub
+}
+ 
+ public String toString() {
+	 return "Name is : "+name+"\nAge is: "+age;
+	 
+ }
+}
---
+++ ToString/Test.java
@@ -0,0 +1,11 @@
+package ToString;
+
+public class Test {
+	public static void main(String[] args) {
+		Person p1= new Person("imam", 22);
+		Person p2 = new Person("cm",23);
+		System.out.println(p1);
+		System.out.println(p2);
+	}
+
+}
